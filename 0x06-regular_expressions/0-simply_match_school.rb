9191941669ae20ff282ea40044c4ecb7daaabee5
meaxzy <olayinkawu@gmail.com>
2023-11-28 11:17:49 -0800

    add: Simple Match

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,19 +1,3 @@
 #!/usr/bin/env ruby
-# Ensure an argument is provided
-
-if ARGV.empty?
-  puts "Usage: #{$PROGRAM_NAME} <text>"
-  exit 1
-end
-
-# Get the argument
-text = ARGV[0]
-
-# Define the regular expression
-regex = /School/
-
-# Match the text against the regex
-match = text.match(regex)
-
-# Output the result
-puts match ? "#{match}$" : "$"
+# checks a word
+puts ARGV[0].scan(/School/).join
